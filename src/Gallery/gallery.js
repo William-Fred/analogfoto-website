f438620e8103d56d915afb6c9724ad91b2e164b3
William Fred <williamfred278@gmail.se>
2023-02-08 19:18:56 +0100

    gallery component

---
+++ src/Gallery/gallery.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./gallery.css";
+import PictureArray from "../Images/picturesArray";
+export default function gallery() {
+  return (
+    <div>
+      <PictureArray />
+    </div>
+  );
+}
